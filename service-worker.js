efd802c721e84254e2e81736c4703ee5b4346f2c
Indrek Lasn <lasnindrek@gmail.com>
2019-06-27 15:05:20 +0200

    init

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+
+// check if the serviceWorker object exists in the navigator object
+if ('serviceWorker' in navigator) {
+
+  // attach event listener  on page l aod
+  window.addEventListener('load', function() {
+
+    // register serviceWorker withthe [service-worker.js] file
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+
+  });
+}
+
+// install event for the service worker
+self.addEventListener('install', e => {
+
+  e.waitUntil(
+    caches.open('site-static').then(cache => {
+      cache.addAll(['/', '/index.html'])
+    })
+  )
+  
+
+})
